82fefcf6aeb206a60516f8547e790f57da11ffd0
threecee <tc@threeceemedia.com>
2020-03-03 00:58:32 -0500

    cleanup

--- index.js
+++ index.js
@@ -1059,6 +1059,20 @@ NeuralNetworkTools.prototype.streamTrainNetwork = async function (params) {
       console.log(MODULE_ID_PREFIX +" streamTrainNetwork | ", data);
     };
 
+    // BRAIN
+    // net.train(data, {
+    //   // Defaults values --> expected validation
+    //   iterations: 20000, // the maximum times to iterate the training data --> number greater than 0
+    //   errorThresh: 0.005, // the acceptable error percentage from training data --> number between 0 and 1
+    //   log: false, // true to use console.log, when a function is supplied it is used --> Either true or a function
+    //   logPeriod: 10, // iterations between logging out --> number greater than 0
+    //   learningRate: 0.3, // scales with delta to effect training rate --> number between 0 and 1
+    //   momentum: 0.1, // scales with next layer's change value --> number between 0 and 1
+    //   callback: null, // a periodic call back that can be triggered while training --> null or function
+    //   callbackPeriod: 10, // the number of iterations through the training data between callback calls --> number greater than 0
+    //   timeout: Infinity, // the max number of milliseconds to train for --> number greater than 0
+    // })
+
     const errorThresh = params.options.error || DEFAULT_BRAIN_TRAIN_ERROR;
     const iterations = params.options.iterations || DEFAULT_BRAIN_TRAIN_ITERATIONS;
     const learningRate = params.options.learningRate || DEFAULT_BRAIN_TRAIN_LEARNING_RATE;
